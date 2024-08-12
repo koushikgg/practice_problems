1b6d0754d4e2bd9edeaa14b4e3e205b5d35ad93c
koushikgg <iamkoushikg@gmail.com>
2024-08-12 12:58:29 +0530

    [koushik]added: time slot calculator problem solved

---
+++ timeSlotCalculator.js
@@ -0,0 +1,25 @@
+function timeSlotCalculator(arr) {
+    output = []
+    for (let i = 0; i < arr.length; i++) {
+        if (i === arr.length - 1) {
+            if (arr[i][1] < 24) {
+                output.push([arr[i][1], 24])
+            }
+            continue
+        }
+        if (i === 0) {
+            if (arr[i][0] !== 0) {
+                output.push([0, arr[i][0]])
+            }
+        }
+        nextTime = arr[i + 1][0]
+        if (arr[i][1] < nextTime) {
+            output.push([arr[i][1], nextTime])
+        }
+    }
+    return output
+}
+
+array = [[10, 12], [14, 15], [16, 23]]
+
+console.log(timeSlotCalculator(array));
