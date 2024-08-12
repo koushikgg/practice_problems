a3a856823e9757615c355ea300d1057297c7a13b
koushikgg <iamkoushikg@gmail.com>
2024-08-12 10:42:39 +0530

    [koushik]added: pyramidArrangement problem solved

--- pyramidArrangement.js
+++ pyramidArrangement.js
@@ -1,11 +1,14 @@
 function pyramid(arr1) {
     let length = arr1.length
     arr1.sort((a, b) => a - b)
+    console.log(arr1);
     for (let i = 1; i < length / 2; i++) {
         temp = arr1[i]
         index = length - i+1
+        console.log(temp,index);
         arr1.splice(index,0,temp)
         arr1.splice(i,1)
+        console.log(arr1);
     }
     return arr1
 }
