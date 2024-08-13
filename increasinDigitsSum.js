c770e680eee32f560e5f16a4fa0565c4537d2fe0
koushikgg <iamkoushikg@gmail.com>
2024-08-13 09:46:28 +0530

    [koushik]added: increase digits sum problem solved

---
+++ increasinDigitsSum.js
@@ -0,0 +1,16 @@
+function increaseDigitsSum(number){
+    output = []
+    function findCombination(start,sum,combinations){
+        if (sum===0){
+            output.push(combinations.join(''))
+        }
+        for (let i = start;i<=9;i++){
+            if (i>sum) break;
+            findCombination(i,sum-i,[...combinations,i])
+        }
+    }
+    findCombination(1,number,[])
+    return output
+}
+let number= 5
+console.log(increaseDigitsSum(number).join('\n'));
